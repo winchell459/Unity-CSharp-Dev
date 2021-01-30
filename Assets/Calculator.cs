2580a78a8f4c71098f50e9addb24041411c03cb7
winchell459 <johncmorris@gmail.com>
2021-01-29 22:39:41 -0800

    Day 1

---
+++ Assets/Calculator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    public Text Display;
+    [SerializeField] private int lastNum;
+    [SerializeField]  private string oper = "";
+    [SerializeField]  private bool operationEntered;
+    //private bool nextNumberAdded;
+    
+    public void NumberButton(int num)
+    {
+        if (operationEntered)
+        {
+            Display.text = num.ToString();
+            operationEntered = false;
+        }
+        else 
+        {
+
+            addToDisplay(num);
+        }
+    }
+
+    public void EnterButton()
+    {
+        if (oper != "") calculate();
+    }
+
+    public void OperationButton(string oper)
+    {
+        if (!operationEntered)
+        {
+            lastNum = int.Parse(Display.text);
+
+        }
+        
+        this.oper = oper;
+        operationEntered = true;
+
+    }
+
+    private void addToDisplay(int num)
+    {
+        if(Display.text== "0") //Display.text.Equals("0")
+        {
+            Display.text = num.ToString();
+        }
+        else
+        {
+            Display.text = Display.text + num.ToString();
+        }
+    }
+
+    private void calculate()
+    {
+        if (oper == "/")
+        {
+            lastNum /= int.Parse(Display.text);
+
+        }
+        else if (oper == "x")
+        {
+            lastNum *= int.Parse(Display.text);
+        }
+        else if (oper == "+")
+        {
+            lastNum += int.Parse(Display.text);
+        }
+        else if (oper == "-")
+        {
+            lastNum -= int.Parse(Display.text);
+        }
+        else
+        {
+            return;
+        }
+        Display.text = lastNum.ToString();
+        operationEntered = false;
+    }
+}
---
+++ Assets/Inputs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inputs : MonoBehaviour
+{
+    public InputField txtA, txtB;
+    public Text lblOutput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CommitButton()
+    {
+        try
+        {
+            int a = int.Parse(txtA.text);
+            int b = int.Parse(txtB.text);
+            lblOutput.text = (a + b).ToString();
+        }
+        catch (System.FormatException)
+        {
+            print("parse error");
+        }
+        
+    }
+
+}
