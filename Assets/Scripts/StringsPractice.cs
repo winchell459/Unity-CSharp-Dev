c12f453051d0a449482301f0cb1d4a236c224ab2
winchell459 <johncmorris@gmail.com>
2021-02-01 14:58:43 -0800

    Datatype review and String methods

---
+++ Assets/Scripts/StringsPractice.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringsPractice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Replace - replaces all instances of a string with another string
+        //Split
+        //Substring
+        //IndexOf
+        //Equals
+
+        string input = "1,009,456";
+
+        string output = "";
+        output = input.Replace(",", "");
+        print(output); //"1009456"
+
+        string[] output2 = new string[2];
+        output2 = input.Split(',');
+        print(output2[1]); // "009"
+
+        
+        string output3 = "";
+        output3 = output.Substring(3);
+        print(output3); //"9456"
+
+        int output4 = 0;
+        string substring = "09";
+        output4 = output.IndexOf(substring);
+        print(output4); //2
+    }
+
+    
+}
